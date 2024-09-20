28c7c10acd930ffd61033fbaf12fac45198400fc
Davilez <josedavid4227@gmail.com>
2024-09-20 13:12:24 -0500

    test

--- src/controllers/v1/tags/getTags.controller.js
+++ src/controllers/v1/tags/getTags.controller.js
@@ -3,6 +3,7 @@ const errorHandler = require("../../../tools/errorHandler");
 const getTagsController = async (req, res) => {
   // * controller for get current tags
   try {
+
     const tags = await tagRepository.get();
     return res.status(200).json({ tags });
   } catch (e) {
