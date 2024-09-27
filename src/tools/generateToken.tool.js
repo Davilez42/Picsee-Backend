c6612985c8007595e058745e96ed5e7ecfb43150
Davilez <josedavid4227@gmail.com>
2024-09-27 14:39:40 -0500

    refactor: jwt

--- src/tools/generateToken.tool.js
+++
@@ -1,7 +0,0 @@
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
-const generateToken = (data) => {
-  return jwt.sign(data, process.env.JWT_KEY_SECRET, { expiresIn: "22h" });
-};
-
-module.exports = generateToken
